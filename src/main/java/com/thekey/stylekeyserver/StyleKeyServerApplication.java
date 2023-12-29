f225e1f22d285e09b578c13c0e6bf8e2ac9e9c4d
jeondui <jeondui@gmail.com>
2023-12-29 20:49:58 +0900

    First commit

---
+++ src/main/java/com/thekey/stylekeyserver/StyleKeyServerApplication.java
@@ -0,0 +1,13 @@
+package com.thekey.stylekeyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleKeyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleKeyServerApplication.class, args);
+	}
+
+}
