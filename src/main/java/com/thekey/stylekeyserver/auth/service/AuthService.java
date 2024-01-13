342737c59970599fde4afb3fe3fd02ed3606ed9e
Subin <rla0010101@gmail.com>
2024-01-13 12:03:02 +0900

    refactor: error message를 enum으로 도메인 별 관리하도록 수정

--- src/main/java/com/thekey/stylekeyserver/auth/service/AuthService.java
+++ src/main/java/com/thekey/stylekeyserver/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.thekey.stylekeyserver.auth.AuthErrorMessage;
 import com.thekey.stylekeyserver.auth.entity.AuthPasswordEntity;
 import com.thekey.stylekeyserver.auth.repository.AuthPasswordRepository;
 import com.thekey.stylekeyserver.auth.service.dto.RegisterPasswordAuthRequest;
@@ -25,7 +26,7 @@ public class AuthService {
         AuthPasswordEntity authEntity = authPasswordJpaRepository.findById(request.getId()).orElse(null);
 
         if (authEntity != null) {
-            throw new DataIntegrityViolationException("이미 가입되었습니다.");
+            throw new DataIntegrityViolationException(AuthErrorMessage.ALREADY_REGISTERED.get());
         }
 
         String encryptedPassword = passwordEncoder.encode(request.getPassword());
--- src/main/java/com/thekey/stylekeyserver/security/SecurityConfig.java
+++ src/main/java/com/thekey/stylekeyserver/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.thekey.stylekeyserver.config;
+package com.thekey.stylekeyserver.security;
 
 
 import org.springframework.context.annotation.Bean;
