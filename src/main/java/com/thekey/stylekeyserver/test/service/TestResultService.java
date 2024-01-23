7ccc377b8a4491258ec9fdebb70d4638cc4cca9e
JWbase <jungwoo1002@gmail.com>
2024-01-23 16:16:36 +0900

    refactor: 스타일포인트 조회 예외 처리 추가

--- src/main/java/com/thekey/stylekeyserver/test/service/TestResultService.java
+++ src/main/java/com/thekey/stylekeyserver/test/service/TestResultService.java
@@ -2,12 +2,14 @@ package com.thekey.stylekeyserver.test.service;
 
 import com.thekey.stylekeyserver.auth.entity.AuthEntity;
 import com.thekey.stylekeyserver.auth.repository.AuthRepository;
+import com.thekey.stylekeyserver.stylepoint.StylePointErrorMessage;
 import com.thekey.stylekeyserver.stylepoint.domain.StylePoint;
 import com.thekey.stylekeyserver.stylepoint.repository.StylePointRepository;
 import com.thekey.stylekeyserver.test.dto.request.TestResultRequest;
 import com.thekey.stylekeyserver.test.dto.response.TestResultResponse;
 import com.thekey.stylekeyserver.test.entity.TestResult;
 import com.thekey.stylekeyserver.test.repository.TestResultRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +25,13 @@ public class TestResultService {
 
     @Transactional
     public TestResultResponse createTestResult(TestResultRequest request) {
+
         AuthEntity user = authRepository.findByUserId(request.getUserId()).orElseThrow();
-        StylePoint stylePoint = stylePointRepository.findById(request.getStylePointId()).orElseThrow();
+
+        StylePoint stylePoint = stylePointRepository.findById(request.getStylePointId())
+            .orElseThrow(() -> new EntityNotFoundException(
+                StylePointErrorMessage.NOT_FOUND_STYLE_POINT.get() + request.getStylePointId()));
+
         TestResult testResult = TestResultRequest.toEntity(user, stylePoint, request);
 
         return TestResultResponse.of(testResultRepository.save(testResult));
