a1b90a54d6683bb7a03de9ae4ea3f2e420d89f98
JWbase <jungwoo1002@gmail.com>
2024-01-24 17:18:32 +0900

    feat: 단건 테스트 결과 삭제 기능 구현

--- src/main/java/com/thekey/stylekeyserver/test/controller/TestController.java
+++ src/main/java/com/thekey/stylekeyserver/test/controller/TestController.java
@@ -10,7 +10,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,12 @@ public class TestController {
     public ResponseEntity<List<TestResultResponse>> getTestResults() {
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
         return ResponseEntity.ok(testResultService.getTestResult(userId));
+    }
+
+    @DeleteMapping("/test-result/{testResultId}")
+    public ResponseEntity<Void> deleteTestResult(@PathVariable Long testResultId) {
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        testResultService.deleteTestResult(testResultId, userId);
+        return ResponseEntity.noContent().build();
     }
 }
