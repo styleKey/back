e647afe0824054390536275a923cd8a0b5824910
jeondui <jeondui@gmail.com>
2024-01-26 18:19:32 +0900

    refactor: dto request/response 분리

--- src/main/java/com/thekey/stylekeyserver/stylepoint/controller/StylePointAdminController.java
+++ src/main/java/com/thekey/stylekeyserver/stylepoint/controller/StylePointAdminController.java
@@ -1,14 +1,15 @@
 package com.thekey.stylekeyserver.stylepoint.controller;
 
 import com.thekey.stylekeyserver.stylepoint.domain.StylePoint;
-import com.thekey.stylekeyserver.stylepoint.dto.StylePointDto;
+import com.thekey.stylekeyserver.stylepoint.dto.request.StylePointRequest;
+import com.thekey.stylekeyserver.stylepoint.dto.response.StylePointResponse;
 import com.thekey.stylekeyserver.stylepoint.service.StylePointAdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,35 +28,39 @@ public class StylePointAdminController {
 
     @GetMapping("/{id}")
     @Operation(summary = "Read One StylePoint", description = "스타일포인트 단건 정보 조회")
-    public ResponseEntity<StylePoint> getStylePoint(@PathVariable Long id) {
+    public ResponseEntity<StylePointResponse> getStylePoint(@PathVariable Long id) {
         Optional<StylePoint> optional = Optional.ofNullable(stylePointAdminService.findById(id));
 
-        return optional.map(stylePoint -> ResponseEntity.ok(stylePoint))
+        return optional.map(stylePoint -> ResponseEntity.ok(StylePointResponse.of(stylePoint)))
                 .orElse(ResponseEntity.notFound().build());
+
     }
 
     @GetMapping
     @Operation(summary = "Read All StylePoint", description = "스타일포인트 전체 정보 조회.")
-    public ResponseEntity<List<StylePoint>> getStylePoints() {
+    public ResponseEntity<List<StylePointResponse>> getStylePoints() {
         List<StylePoint> stylePoints = stylePointAdminService.findAll();
 
-        return Optional.of(stylePoints)
-                .filter(list -> !list.isEmpty())
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NO_CONTENT).body(stylePoints));
+        if (stylePoints.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(stylePoints.stream()
+                .map(StylePointResponse::of)
+                .collect(Collectors.toList()));
+
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Update StylePoint", description = "스타일포인트 정보 수정")
-    public ResponseEntity<StylePoint> update(@PathVariable Long id,
-                                             @RequestBody StylePointDto requestDto) {
+    public ResponseEntity<StylePointResponse> update(@PathVariable Long id,
+                                                     @RequestBody StylePointRequest requestDto) {
 
         if (id == null) {
             return ResponseEntity.badRequest().build();
         }
 
-        Optional<StylePoint> optional = Optional.ofNullable(stylePointAdminService.update(id, requestDto));
-        return optional.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        StylePoint updated = stylePointAdminService.update(id, requestDto);
+        return ResponseEntity.ok(StylePointResponse.of(updated));
     }
 }
