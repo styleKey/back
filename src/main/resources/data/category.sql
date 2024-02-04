5d7d27683a521041d9401edc286801bd3034dee8
jeondui <jeondui@gmail.com>
2024-02-04 17:56:50 +0900

    feat: H2-Database 멀티 데이터소스 사용하도록 설정 수정

---
+++ src/main/resources/data/category.sql
@@ -0,0 +1,13 @@
+insert into category (category_title) values ('TOP');
+
+insert into category (category_title) values ('OUTER');
+
+insert into category (category_title) values ('DRESS');
+
+insert into category (category_title) values ('BOTTOM');
+
+insert into category (category_title) values ('BAG');
+
+insert into category (category_title) values ('SHOES');
+
+insert into category (category_title) values ('ACC');
---
+++ src/main/resources/data/style-point.sql
@@ -0,0 +1,31 @@
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Unique', '변화하는 트렌드를 반영하여 평범하지 않고 개성있는 디테일을 추구하는 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%8B%E1%85%B2%E1%84%82%E1%85%B5%E1%84%8F%E1%85%B3%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Street', '격식을 갖추지 않고 길거리에서 편하게 입을 수 있는 힙한 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%89%E1%85%B3%E1%84%90%E1%85%B3%E1%84%85%E1%85%B5%E1%86%BA%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Modern', '장식적인 것 없이 깔끔하고 심플하며 직선적인 실루엣을 추구하는 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%86%E1%85%A9%E1%84%83%E1%85%A5%E1%86%AB%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Normal', '일상적이고 평범한 착장이 무난하지 않도록 센스있는 포인트가 들어간 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%82%E1%85%A9%E1%84%86%E1%85%A5%E1%86%AF%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Lovely', '사랑스러운 소녀같이 귀엽고 로맨틱하면서 여성스러운 무드를 강조한 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%85%E1%85%A5%E1%84%87%E1%85%B3%E1%86%AF%E1%84%85%E1%85%B5%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.jpg');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Retro', '1990-2000년대의 감성을 재해석하여 오래된 듯한 멋진 느낌이 드는 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%85%E1%85%A6%E1%84%90%E1%85%B3%E1%84%85%E1%85%A9%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.jpg');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Glam', '섹시함이 강조되는 화려하고 여성스러운 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%80%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A2%E1%86%B7%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
+
+insert into style_point (style_point_title, style_point_description, style_point_image)
+values ('Active', '스포츠웨어와 일상복의 경계를 허물고 활동적인 이미지를 표현하는 스타일',
+        'https://stylekeybucket.s3.ap-northeast-2.amazonaws.com/stylepoint/%E1%84%8B%E1%85%A6%E1%86%A8%E1%84%90%E1%85%B5%E1%84%87%E1%85%B3%E1%84%91%E1%85%A9%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%90%E1%85%B3.png');
