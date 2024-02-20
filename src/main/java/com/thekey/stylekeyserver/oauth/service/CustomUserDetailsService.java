5f0f6ed6937d1c5cabe44909059fb741cf0ef07e
Subin <rla0010101@gmail.com>
2024-02-20 23:13:20 +0900

    feat: CustomOAuth2UserService, CustomUserDetailsService 구현

---
+++ src/main/java/com/thekey/stylekeyserver/oauth/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.thekey.stylekeyserver.oauth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.thekey.stylekeyserver.auth.entity.User;
+import com.thekey.stylekeyserver.auth.repository.UserRepository;
+import com.thekey.stylekeyserver.oauth.entity.UserPrincipal;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Can not find username.");
+        }
+        return UserPrincipal.create(user);
+    }
+}
