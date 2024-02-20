345292f6dd1af42a44b8dc249af82a5b45c25048
Subin <rla0010101@gmail.com>
2024-02-20 21:40:27 +0900

    기존 security, auth, oauth 코드 삭제

--- src/main/java/com/thekey/stylekeyserver/auth/domain/oauth/OAuthInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package com.thekey.stylekeyserver.auth.domain.oauth;
-
-import com.thekey.stylekeyserver.auth.domain.enums.OAuthProvider;
-
-public interface OAuthInfoResponse {
-    String getEmail();
-    String getNickname();
-    OAuthProvider getOAuthProvider();
-}
