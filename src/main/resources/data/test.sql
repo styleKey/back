cbbd6962ec0bd56f855429f1562419b6fb6448f7
JWbase <jungwoo1002@gmail.com>
2024-02-20 22:55:11 +0900

    refactor: test.sql 수정

--- src/main/resources/data/test.sql
+++ src/main/resources/data/test.sql
@@ -1,73 +1,33 @@
 -- TestQuestion
-INSERT INTO test_question (test_question_content) values ('여러분이 가장 좋아하는 계절은 언제인가요?');
+INSERT INTO test_question (test_question_content)
+values ('여러분이 가장 좋아하는 계절은 언제인가요?');
 
-INSERT INTO test_question (test_question_content) values ('평소 여가시간을 어떻게 보내시나요?');
-
-INSERT INTO test_question (test_question_content) values ('이 아이템들 중 가장 가지고 싶은 것은 무엇인가요?');
-
-INSERT INTO test_question (test_question_content) values ('옷을 선택할 때 어떤 요소에 가장 중점을 두시나요?');
-
-INSERT INTO test_question (test_question_content) values ('요즘 인기 연예인들 중 가장 워너비에 가까운 사람은?');
-
-INSERT INTO test_question (test_question_content) values ('평소 어떤 헤어스타일을 선호 하시나요?');
-
-INSERT INTO test_question (test_question_content) values ('요즘 어떤 노래를 듣고 계세요?');
-
-INSERT INTO test_question (test_question_content) values ('옷장에 가장 많이 있는 색은 무엇인가요?');
-
-INSERT INTO test_question (test_question_content) values ('어떤 신발을 많이 착용하시나요?');
-
-INSERT INTO test_question (test_question_content) values ('옷을 주로 사시는 곳이 어디인가요?');
-
-INSERT INTO test_question (test_question_content) values ('당신의 sns피드 분위기는 어느 것에 제일 가깝나요?');
+INSERT INTO test_question (test_question_content)
+values ('평소 여가시간을 어떻게 보내시나요?');
 
 -- TestAnswer
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('하얀 눈이 오는 겨울', '이미지', 1, 3);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('벚꽃 휘날리는 봄', '이미지', 1, 5);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('쨍쨍한 여름', '이미지', 1, 8);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('감성적인 가을', '이미지', 1, 6);
+-- 1번 선택지
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('봄',  1);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('깔끔한 감성 카페 투어하기', '이미지', 2, 3);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('뜨개질하거나 다이어리 꾸미기', '이미지', 2, 5);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('몸을 많이 움직이는 운동하기', '이미지', 2, 8);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('CD나 LP판으로 옛날 노래 감상하기', '이미지', 2, 6);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('여름', 1);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('현대적이고 세련된 장식의 목걸이', '이미지', 3, 3);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('레트로한 느낌의 베레모', '이미지', 3, 5);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('프릴 장식의 사랑스러운 스커트', '이미지', 3, 8);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('활동하기 편한 운동복', '이미지', 3, 6);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('가을', 1);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('깔끔하고 단순한 패턴', '이미지', 4, 3);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('플라워 패턴, 풍성한 모양', '이미지', 4, 5);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('땀이 잘 흡수되는 편안한 소재', '이미지', 4, 8);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('레트로하고 힙한 디자인', '이미지', 4, 6);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('겨울', 1);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('크리스탈', '이미지', 5, 3);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '이미지', 5, 5);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('미노이', '이미지', 5, 8);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('김보라', '이미지', 5, 6);
+-- 2번 선택지
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('잠', 2);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('미디움 기장의 자연스럽고 간편한 스타일', '이미지', 6, 4);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '세련된 느낌의 업스타일 혹은 긴머리 웨이브 또는 생머리', 6, 7);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('힙한 느낌의 탈색머리나 숏컷 스타일', '이미지', 6, 2);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('층이 엄청난 허쉬컷 혹은 개성 넘치는 브릿지', '이미지', 6, 1);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('게임', 2);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('마음이 편해지는 클래식', '이미지', 7, 4);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '나도 모르게 치명적인 척하게 되는 섹시한 노래', 7, 7);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('힙합 이즈 마이 라이프', '이미지', 7, 2);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('나만 알고 있는 숨어서 듣게 되는 독특한 노래', '이미지', 7, 1);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('독서', 2);
 
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('심플한 검정, 흰색, 회색의 무채색', '이미지', 8, 4);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '화려하지만 세련된 검정, 은색, 보라', 8, 7);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('느낌있는 청색, 레드, 옐로, 카키', '이미지', 8, 2);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('독특하고 눈에 띄는 레오파드, 네온색 혹은 패턴 컬러', '이미지', 8, 1);
-
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('깔끔한 컨버스나 로퍼', '이미지', 9, 4);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '섹시한 하이힐이나 롱부츠', 9, 7);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('유명 브랜드의 인기 많은 운동화', '이미지', 9, 2);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('처음 보는 스타일의 눈에 띄는 신발', '이미지', 9, 1);
-
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('무난한 유명 옷사이트 인기순위', '이미지', 10, 4);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('츄', '외국 핫 걸들을 위한 사이트', 10, 7);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('길거리에 꼭 있는 브랜드 편집샵', '이미지', 10, 2);
-INSERT INTO test_answer (test_answer_content, test_answer_image, test_question_id, style_point_id) VALUES ('다른 사람들은 모르는 나만의 단골 옷가게', '이미지', 10, 1);
+INSERT INTO test_answer (test_answer_content, test_question_id)
+VALUES ('영화', 2);
