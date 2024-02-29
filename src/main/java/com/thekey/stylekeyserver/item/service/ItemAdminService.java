781733b21adafd11694d51ff753a04e180a68975
jeondui <jeondui@gmail.com>
2024-02-29 20:41:03 +0900

    feat: Item 비즈니스 로직 수정

--- src/main/java/com/thekey/stylekeyserver/item/service/ItemAdminService.java
+++ src/main/java/com/thekey/stylekeyserver/item/service/ItemAdminService.java
@@ -3,18 +3,20 @@ package com.thekey.stylekeyserver.item.service;
 import com.thekey.stylekeyserver.coordinatelook.service.CoordinateLookAdminService;
 import com.thekey.stylekeyserver.item.domain.Item;
 import com.thekey.stylekeyserver.item.dto.request.ItemRequest;
+import java.io.IOException;
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ItemAdminService {
 
-    Item create(ItemRequest requestDto);
+    Item create(ItemRequest requestDto, MultipartFile imageFile) throws IOException;
 
     Item findById(Long id);
 
     List<Item> findAll();
     List<Item> findAllByCoordinateLookId(Long id, CoordinateLookAdminService coordinateLookAdminService);
 
-    Item update(Long id, ItemRequest requestDto);
+    Item update(Long coordinateLookId, Long itemId, ItemRequest requestDto, MultipartFile imageFile) throws IOException;
 
     void delete(Long id);
 
--- src/main/java/com/thekey/stylekeyserver/item/service/ItemAdminServiceImpl.java
+++ src/main/java/com/thekey/stylekeyserver/item/service/ItemAdminServiceImpl.java
@@ -6,69 +6,114 @@ import com.thekey.stylekeyserver.category.domain.Category;
 import com.thekey.stylekeyserver.category.service.CategoryService;
 import com.thekey.stylekeyserver.coordinatelook.domain.CoordinateLook;
 import com.thekey.stylekeyserver.coordinatelook.service.CoordinateLookAdminService;
+import com.thekey.stylekeyserver.image.domain.Image;
+import com.thekey.stylekeyserver.image.domain.Type;
+import com.thekey.stylekeyserver.image.repository.ImageRepository;
 import com.thekey.stylekeyserver.item.ItemErrorMessage;
 import com.thekey.stylekeyserver.item.domain.Item;
 import com.thekey.stylekeyserver.item.dto.request.ItemRequest;
 import com.thekey.stylekeyserver.item.repository.ItemRepository;
+import com.thekey.stylekeyserver.s3.S3Service;
+import java.io.IOException;
 import java.util.List;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ItemAdminServiceImpl implements ItemAdminService {
 
     private final ItemRepository itemRepository;
+    private final ImageRepository imageRepository;
     private final BrandAdminService brandAdminService;
     private final CategoryService categoryService;
+    private final S3Service s3Service;
 
     @Override
-    public Item create(ItemRequest requestDto) {
+    @Transactional
+    public Item create(ItemRequest requestDto, MultipartFile imageFile) throws IOException {
+        Image image = s3Service.uploadFile(imageFile, Type.ITEM);
+        imageRepository.save(image);
         Category category = categoryService.findById(requestDto.getCategoryId());
         Brand brand = brandAdminService.findById(requestDto.getBrandId());
-        return itemRepository.save(requestDto.toEntity(brand, category));
+
+        Item item = requestDto.toEntity(brand, category);
+        item.setImage(image);
+
+        return itemRepository.save(item);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Item findById(Long id) {
         return itemRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(ItemErrorMessage.NOT_FOUND_ITEM.get() + id));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Item> findAll() {
         return itemRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Item> findAllByCoordinateLookId(Long id, CoordinateLookAdminService coordinateLookAdminService) {
         CoordinateLook coordinateLook = coordinateLookAdminService.findById(id);
         return itemRepository.findItemByCoordinateLook(coordinateLook);
     }
 
     @Override
-    public Item update(Long id, ItemRequest requestDto) {
-        Item item = itemRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(ItemErrorMessage.NOT_FOUND_ITEM.get() + id));
+    @Transactional
+    public Item update(Long coordinateLookId, Long itemId, ItemRequest requestDto, MultipartFile imageFile)
+            throws IOException {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new EntityNotFoundException(ItemErrorMessage.NOT_FOUND_ITEM.get() + itemId));
+
+        if (!itemRepository.existsByCoordinateLookId(coordinateLookId)) {
+            throw new IllegalArgumentException(ItemErrorMessage.NOT_FOUND_ITEM.get());
+        }
 
         Category category = categoryService.findById(requestDto.getCategoryId());
         Brand brand = brandAdminService.findById(requestDto.getBrandId());
 
+        if (!imageFile.isEmpty()) {
+            Image oldImage = item.getImage();
+            if (oldImage != null) {
+                oldImage.setUnused();
+                imageRepository.save(oldImage);
+
+                Image newImage = s3Service.uploadFile(imageFile, Type.ITEM);
+                imageRepository.save(newImage);
+                item.setImage(newImage);
+                itemRepository.save(item);
+            }
+        }
+
         item.update(requestDto.getTitle(),
                 requestDto.getSales_link(),
-                requestDto.getImage(),
-                requestDto.toEntity(brand, category).getBrand(),
-                requestDto.toEntity(brand, category).getCategory());
+                brand,
+                category);
 
         return item;
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
+        Item item = itemRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ItemErrorMessage.NOT_FOUND_ITEM.get() + id));
+
+        Image image = item.getImage();
+
+        if (image != null) {
+            image.setUnused();
+            imageRepository.save(image);
+        }
         itemRepository.deleteById(id);
     }
 
