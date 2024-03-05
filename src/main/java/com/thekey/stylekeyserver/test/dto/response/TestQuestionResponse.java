575c6fafaf460993675334ba8ecbe15e7da6ffe9
JWbase <jungwoo1002@gmail.com>
2024-03-06 00:08:22 +0900

    [feat/#49] refactor: TestQuestionResponse ImageUrl 추가

--- src/main/java/com/thekey/stylekeyserver/test/dto/response/TestQuestionResponse.java
+++ src/main/java/com/thekey/stylekeyserver/test/dto/response/TestQuestionResponse.java
@@ -13,19 +13,23 @@ public class TestQuestionResponse {
 
     private Long questionId;
     private String content;
+    private String imageUrl;
     private List<TestAnswerResponse> answers;
 
     @Builder
-    private TestQuestionResponse(Long questionId, String content, List<TestAnswerResponse> answers) {
+    private TestQuestionResponse(Long questionId, String content, String imageUrl, List<TestAnswerResponse> answers) {
         this.questionId = questionId;
         this.content = content;
+        this.imageUrl = imageUrl;
         this.answers = answers;
     }
+
 
     public static TestQuestionResponse of(TestQuestion testQuestion) {
         return TestQuestionResponse.builder()
             .questionId(testQuestion.getId())
             .content(testQuestion.getContent())
+            .imageUrl(testQuestion.getImageUrl())
             .answers(testQuestion.getTestAnswers().stream()
                 .map(TestAnswerResponse::of)
                 .toList())
