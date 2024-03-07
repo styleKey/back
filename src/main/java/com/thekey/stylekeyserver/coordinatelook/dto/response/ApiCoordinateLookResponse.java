425bb152d7a5df404f62cbf07de4c922cb9bfb59
jeondui <jeondui@gmail.com>
2024-03-08 00:12:13 +0900

    feat: 좋아요 개수 포함된 dto 추가

--- src/main/java/com/thekey/stylekeyserver/coordinatelook/dto/response/ApiCoordinateLookResponse.java
+++ src/main/java/com/thekey/stylekeyserver/coordinatelook/dto/response/ApiCoordinateLookResponse.java
@@ -1,5 +1,7 @@
-package com.thekey.stylekeyserver.like.dto.response;
+package com.thekey.stylekeyserver.coordinatelook.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.thekey.stylekeyserver.coordinatelook.domain.CoordinateLook;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -10,7 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class LikeCoordinateLookResponse {
+@JsonNaming(SnakeCaseStrategy.class)
+public class ApiCoordinateLookResponse {
 
     @Schema(description = "코디룩 ID")
     private Long id;
@@ -24,22 +27,28 @@ public class LikeCoordinateLookResponse {
     @Schema(description = "스타일포인트 ID")
     private Long stylePointId;
 
+    @Schema(description = "코디룩 좋아요 개수")
+    private Integer likeCount;
+
     @Builder
-    public LikeCoordinateLookResponse(Long id, String title, String coordinateLookImageUrl, Long stylePointId) {
+    public ApiCoordinateLookResponse(Long id, String title, String coordinateLookImageUrl, Long stylePointId,
+                                     Integer likeCount) {
         this.id = id;
         this.title = title;
         this.coordinateLookImageUrl = coordinateLookImageUrl;
         this.stylePointId = stylePointId;
+        this.likeCount = likeCount;
     }
 
-    public static LikeCoordinateLookResponse of(CoordinateLook coordinateLook) {
+    public static ApiCoordinateLookResponse of(CoordinateLook coordinateLook, Integer likeCount) {
         String imageUrl = coordinateLook.getImage().getUrl();
 
-        return LikeCoordinateLookResponse.builder()
+        return ApiCoordinateLookResponse.builder()
                 .id(coordinateLook.getId())
                 .title(coordinateLook.getTitle())
                 .coordinateLookImageUrl(imageUrl)
                 .stylePointId(coordinateLook.getStylePoint().getId())
+                .likeCount(likeCount)
                 .build();
     }
 }
