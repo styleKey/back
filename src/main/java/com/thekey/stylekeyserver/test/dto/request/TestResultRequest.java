6743924ed721c324a25998ad69d09cf534be5abd
JWbase <jungwoo1002@gmail.com>
2024-03-10 15:46:21 +0900

    [feat/#63] refactor: answerIds 검증 추가

--- src/main/java/com/thekey/stylekeyserver/test/dto/request/TestResultRequest.java
+++ src/main/java/com/thekey/stylekeyserver/test/dto/request/TestResultRequest.java
@@ -2,6 +2,7 @@ package com.thekey.stylekeyserver.test.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.thekey.stylekeyserver.common.annotation.ExactSize;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,9 @@ import lombok.Setter;
 @JsonNaming(SnakeCaseStrategy.class)
 public class TestResultRequest {
 
+    private static final String VALIDATE_ANSWER_SIZE = "총 8개의 응답을 보내야합니다. 응답갯수를 확인하세요.";
+
+    @ExactSize(size = 8, message = VALIDATE_ANSWER_SIZE)
     private List<Long> answerIds;
 
     @Builder
