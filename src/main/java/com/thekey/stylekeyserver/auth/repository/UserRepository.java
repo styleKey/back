059758be2214d674ad3fa2be57cffa2f29bf3712
JWbase <jungwoo1002@gmail.com>
2024-03-10 17:00:47 +0900

    [feat/#63] refactor: findByUserId 반환 타입 Optional로 변경

--- src/main/java/com/thekey/stylekeyserver/auth/repository/UserRepository.java
+++ src/main/java/com/thekey/stylekeyserver/auth/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.thekey.stylekeyserver.auth.repository;
 
+import com.thekey.stylekeyserver.auth.entity.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.thekey.stylekeyserver.auth.entity.User;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
+
+    Optional<User> findByUserId(String userId);
 }
--- src/main/java/com/thekey/stylekeyserver/test/service/TestResultService.java
+++ src/main/java/com/thekey/stylekeyserver/test/service/TestResultService.java
@@ -1,5 +1,6 @@
 package com.thekey.stylekeyserver.test.service;
 
+import static com.thekey.stylekeyserver.common.exception.ErrorCode.AUTHENTICATED_FAIL;
 import static com.thekey.stylekeyserver.common.exception.ErrorCode.TEST_ANSWER_NOT_FOUND;
 import static com.thekey.stylekeyserver.common.exception.ErrorCode.TEST_RESULT_NOT_FOUND;
 import static com.thekey.stylekeyserver.common.exception.ErrorCode.UNAUTHORIZED_TEST_RESULT;
@@ -32,29 +33,16 @@ public class TestResultService {
 
     @Transactional
     public Long createTestResult(TestResultRequest request, String userId) {
-        User user = userRepository.findByUserId(userId);
+        User user = findUser(userId);
         Map<StylePoint, Integer> stylePointScores = calculateStylePointScore(request);
         TestResult testResult = TestResult.create(user, stylePointScores);
-
         TestResult savedTestResult = testResultRepository.save(testResult);
         return savedTestResult.getId();
     }
 
-    private Map<StylePoint, Integer> calculateStylePointScore(TestResultRequest request) {
-        return request.getAnswerIds().stream()
-            .map(answerId -> testAnswerRepository.findById(answerId)
-                .orElseThrow(() -> new ApiException(TEST_ANSWER_NOT_FOUND)))
-            .flatMap(testAnswer -> testAnswer.getTestAnswerDetails().stream())
-            .collect(Collectors.toMap(
-                TestAnswerDetail::getStylePoint,
-                TestAnswerDetail::getScore,
-                Integer::sum));
-    }
-
     public List<TestResultResponse> getTestResults(String userId) {
-        User user = userRepository.findByUserId(userId);
+        User user = findUser(userId);
         List<TestResult> testResults = testResultRepository.findAllByUser(user);
-
         return testResults.stream()
             .map(TestResultResponse::of)
             .toList();
@@ -69,13 +57,29 @@ public class TestResultService {
 
     @Transactional
     public void deleteTestResult(Long testResultId, String userId) {
-        User user = userRepository.findByUserId(userId);
+        User user = findUser(userId);
         testResultRepository.deleteByUserAndId(user, testResultId);
+    }
+
+    private User findUser(String userId) {
+        return userRepository.findByUserId(userId)
+            .orElseThrow(() -> new ApiException(AUTHENTICATED_FAIL));
     }
 
     private void validateOwner(String userId, TestResult testResult) {
         if (!testResult.isOwner(userId)) {
             throw new ApiException(UNAUTHORIZED_TEST_RESULT);
         }
+    }
+
+    private Map<StylePoint, Integer> calculateStylePointScore(TestResultRequest request) {
+        return request.getAnswerIds().stream()
+            .map(answerId -> testAnswerRepository.findById(answerId)
+                .orElseThrow(() -> new ApiException(TEST_ANSWER_NOT_FOUND)))
+            .flatMap(testAnswer -> testAnswer.getTestAnswerDetails().stream())
+            .collect(Collectors.toMap(
+                TestAnswerDetail::getStylePoint,
+                TestAnswerDetail::getScore,
+                Integer::sum));
     }
 }
