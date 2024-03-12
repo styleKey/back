b13e75f9675e6259e0ff7cc531d28d42d9a19764
JWbase <jungwoo1002@gmail.com>
2024-03-12 21:10:00 +0900

    [feat/#76] refactor: TestAnswer 조회시 Details fetch join으로 N + 1 문제 해결

--- src/main/java/com/thekey/stylekeyserver/test/repository/TestAnswerRepository.java
+++ src/main/java/com/thekey/stylekeyserver/test/repository/TestAnswerRepository.java
@@ -3,11 +3,13 @@ package com.thekey.stylekeyserver.test.repository;
 import com.thekey.stylekeyserver.test.entity.TestAnswer;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestAnswerRepository extends JpaRepository<TestAnswer, Long> {
 
+    @Query("SELECT ta FROM TestAnswer ta JOIN FETCH ta.testAnswerDetails WHERE ta.id IN :ids")
     List<TestAnswer> findByIdIn(@Param("ids") List<Long> ids);
 }
