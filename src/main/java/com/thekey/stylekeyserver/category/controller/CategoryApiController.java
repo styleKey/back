f62774e904bb7d5055815185a854941292acdcdb
jeondui <jeondui@gmail.com>
2024-03-14 00:53:35 +0900

    refactor: Category API 리팩토링

--- src/main/java/com/thekey/stylekeyserver/category/controller/CategoryApiController.java
+++ src/main/java/com/thekey/stylekeyserver/category/controller/CategoryApiController.java
@@ -2,12 +2,14 @@ package com.thekey.stylekeyserver.category.controller;
 
 import com.thekey.stylekeyserver.category.domain.Category;
 import com.thekey.stylekeyserver.category.service.CategoryService;
+import com.thekey.stylekeyserver.common.exception.ApiResponse;
+import com.thekey.stylekeyserver.common.exception.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,23 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/categories")
-public class CategoryController {
+public class CategoryApiController {
 
     private final CategoryService categoryService;
 
     @GetMapping("/{id}")
     @Operation(summary = "Read One Category", description = "카테고리 정보 단건 조회")
-    public ResponseEntity<Category> getCategory(@PathVariable Long id) {
+    public ApiResponse<Category> getCategory(@PathVariable Long id) {
         Optional<Category> optional = Optional.ofNullable(categoryService.findById(id));
 
         return optional
-                .map(category -> ResponseEntity.ok(category))
-                .orElse(ResponseEntity.notFound().build());
+                .map(ApiResponse::ok)
+                .orElse(ApiResponse.fail(HttpStatus.BAD_REQUEST, ErrorCode.CATEGORY_NOT_FOUND.getMessage()));
     }
 
     @GetMapping
     @Operation(summary = "Read All Categories", description = "카테고리 정보 전체 조회")
-    public ResponseEntity<List<Category>> getCategories() {
-        return ResponseEntity.ok(categoryService.findAll());
+    public ApiResponse<List<Category>> getCategories() {
+        return ApiResponse.ok(categoryService.findAll());
     }
 }
