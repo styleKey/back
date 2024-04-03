832460db402f9b9d2c960b6a9bbdc4dd7437e177
jeondui <jeondui@gmail.com>
2024-04-03 14:14:51 +0900

    feat: 서비스 페이지 코디룩 전체 조회 무한 스크롤 기능 구현
    - 좋아요 순으로 정렬

--- src/main/java/com/thekey/stylekeyserver/coordinatelook/service/CoordinateLookService.java
+++ src/main/java/com/thekey/stylekeyserver/coordinatelook/service/CoordinateLookService.java
@@ -1,20 +1,27 @@
 package com.thekey.stylekeyserver.coordinatelook.service;
 
+import com.thekey.stylekeyserver.coordinatelook.dto.response.ApiCoordinateLookPageResponse;
 import com.thekey.stylekeyserver.coordinatelook.entity.CoordinateLook;
 import com.thekey.stylekeyserver.coordinatelook.dto.response.ApiCoordinateLookDetailsResponse;
 import com.thekey.stylekeyserver.coordinatelook.dto.response.ApiCoordinateLookResponse;
+import com.thekey.stylekeyserver.coordinatelook.repository.CoordinateLookRepository;
 import com.thekey.stylekeyserver.like.service.LikeCoordinateLookService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CoordinateLookService {
 
     private final CoordinateLookAdminService coordinateLookAdminService;
     private final LikeCoordinateLookService likeCoordinateLookService;
+    private final CoordinateLookRepository coordinateLookRepository;
 
     public ApiCoordinateLookDetailsResponse getCoordinateLookWithLikes(Long id) {
         CoordinateLook coordinateLook = coordinateLookAdminService.findById(id);
@@ -22,10 +29,13 @@ public class CoordinateLookService {
         return ApiCoordinateLookDetailsResponse.of(coordinateLook, likeCount, coordinateLook.getItems());
     }
 
-    public List<ApiCoordinateLookResponse> getAllCoordinateLooksWithLikes() {
-        return coordinateLookAdminService.findAll().stream()
+    public ApiCoordinateLookPageResponse getAllCoordinateLooksWithLikes(Pageable pageable) {
+        Slice<CoordinateLook> coordinateLooks = coordinateLookRepository.findAll(pageable);
+        List<ApiCoordinateLookResponse> responses = coordinateLooks.getContent().stream()
                 .map(this::buildApiCoordinateLookResponse)
-                .collect(Collectors.toList());
+                .toList();
+
+        return ApiCoordinateLookPageResponse.fromSlice(responses, coordinateLooks.hasNext());
     }
 
     public List<ApiCoordinateLookResponse> getCoordinateLookWithLikesByStylePointId(Long stylePointId) {
